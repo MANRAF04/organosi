99c386b4cb01029f236a4a4263bc409724912e81
manraf <45131457+MANRAF04@users.noreply.github.com>
2023-11-07 00:31:45 +0200

    Add files via upload

--- lab2/ask2.asm
+++ lab2/ask2.asm
@@ -1,91 +1,94 @@
+.macro print_int (%x)
+li $v0, 1
+add $a0, $zero, %x
+syscall
+.end_macro
+
+.macro READ_INT
+li $v0, 5
+syscall
+.end_macro
+
+.macro print_str (%str)
 .data
-giveN1: .asciiz "Please give N1:\n"
-giveN2: .asciiz "Please give N2:\n"
-
-result1: .asciiz "The max final union of ranges is ["
-result2: .asciiz "].\n"
-
+myLabel: .asciiz %str
+	.text
+	li $v0, 4
+	la $a0, myLabel
+	syscall
+.end_macro
+.data
+array:
+	.byte 0x70,0x8C,0xF3,0x82,0x1B,0x9D,0x52,0x3C,0x46
+	
 .text
 .globl main
 
 main:
-	li $s0, -1	#min
-	li $s1, -1 	#max
-	j loop
+	print_str("Enter pointer:\n")
+	READ_INT
+	move $s0, $v0
+	
+	print_str("Enter offset:\n")
+	READ_INT
+	move $s1, $v0
+	
+	print_str("Enter nbits:\n")
+	READ_INT
+	move $s2, $v0
+	
+    la $s3, array
+    add $s3,$s3,$s0
 
-loop:
-	li $v0,4
-	la $a0,giveN1  		#prints message to take N1
-	syscall
-	
-	li $v0,5
-	syscall			#Takes N1
-	move $t0,$v0
-	bltz $t0,print
-		
-	li $v0,4
-	la $a0,giveN2		#prints message to take N2
-	syscall	
-	
-	li $v0,5
-	syscall			#Takes N2
-	move $t1,$v0
-	bltz $t1,print
-	
-	bgt $t0, $s0, check_kpN2
-	blt $t0, $s1, kp_N1
-		
-	j no_or_full_epi
+	move $a0,$s3
+	move $a1,$s1
+	move $a2,$s2
 
+    jal bits_read
 
-check_kpN2:
-	blt $t0, $s1, kp_N1
-	j no_or_full_epi
+    move $t0,$v0
+    li $t1,1
+    sll $t1,$t1,31
+    printBin:
+    	beqz $t1,end
+    	and $t2,$t0,$t1
+    	srl $t1,$t1,1
+    	beqz $t2,printZero
+    	print_int(1)
+    	j printBin
+    	printZero:
+    	print_int(0)
+    	
+    	j printBin
+end:
+	li $v0, 10
+	syscall
 
-kp_N1:
-	bgt $t0, $s0, kp_N2
-	move $s0, $t0
-	j kp_N2
-
-kp_N2:
-	bgt $s1, $t1, loop
-	move $s1, $t1
-	j loop
-
-no_or_full_epi:
-	sub $t2, $s1, $s0
-	sub $t3 $t1, $t0
-	
-	bgt $t2,$t3, loop
-	move $s0, $t0
-	move $s1, $t1
-	j loop
-
-print:
-	
-	bltz $s0, end
-	bltz $s1, end
-	
-	li $v0, 4
-	la $a0,result1		#Print max range
-	syscall
-	
-	li $v0,1
-	move $a0,$s0
-	syscall
-	
-	li $v0,11
-	li $a0,','
-	syscall
-	
-	li $v0,1
-	move $a0,$s1
-	syscall
-	
-	li $v0,4
-	la $a0,result2
-	syscall
-		
-end:	
-	la $v0,10		#Exits Program
-	syscall
+bits_read:
+    li $t9, 0   #counter
+    li $v0, 0   #result
+    loop:
+        lb		$t1, ($a0)
+        li		$t0, 128
+        srlv		$t0, $t0, $a1
+        and		$t3, $t1, $t0
+        addi		$a1,$a1,1
+        bnez    $t3, changeRes
+        rest:
+        addi    $t9,$t9,1
+        bge	$t9,$a2,exit
+        bgtz	$t0, loop
+        addi	$t9,$t9,-1
+        
+        li	$a1,0
+        addi 	$a0,$a0,1
+        blt     $t9,$a2,loop
+        exit:
+        jr      $ra
+    changeRes:
+        sub $t7,$a2,$t9
+        addi $t7,$t7,-1
+        li $t6,1
+        sllv $t6,$t6,$t7
+        or $v0,$v0,$t6
+        j rest
