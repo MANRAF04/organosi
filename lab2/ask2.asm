0f4da023a7340c698210a2d6d8956ba3050e1605
Zachariadis Charis <124374442+czachariadis@users.noreply.github.com>
2023-11-10 00:29:43 +0200

    Update ask2.asm
    with comments

--- lab2/ask2.asm
+++ lab2/ask2.asm
@@ -1,3 +1,4 @@
+# MACROS
 .macro print_int (%x)
 li $v0, 1
 add $a0, $zero, %x
@@ -17,12 +18,15 @@ myLabel: .asciiz %str
 	la $a0, myLabel
 	syscall
 .end_macro
+
+
 .data
 array:
 	.byte 0x70,0x8C,0xF3,0x82,0x1B,0x9D,0x52,0x3C,0x46
 	
 .text
 .globl main
+
 
 main:
 	print_str("Enter pointer:\n")
@@ -37,58 +41,59 @@ main:
 	READ_INT
 	move $s2, $v0
 	
-    la $s3, array
-    add $s3,$s3,$s0
+    la $s3, array       
+    add $s3,$s3,$s0     # Load the beggining array element
 
 	move $a0,$s3
-	move $a1,$s1
-	move $a2,$s2
+	move $a1,$s1        # Prepare the arguments for the function call
+	move $a2,$s2        
 
     jal bits_read
 
     move $t0,$v0
-    li $t1,1
-    sll $t1,$t1,31
+    li $t1,1            
+    sll $t1,$t1,31      # create a 32-bit mask with "1" only in the beginning
     printBin:
-    	beqz $t1,end
-    	and $t2,$t0,$t1
-    	srl $t1,$t1,1
-    	beqz $t2,printZero
-    	print_int(1)
+    	beqz $t1,end    # if mask == 0 we reached the end
+    	and $t2,$t0,$t1 # perform and with the mask that focuses on the current bit
+    	srl $t1,$t1,1   # move the "1" to the next bit
+    	beqz $t2,printZero  # if the and is full of zeros the bit is "0"
+    	print_int(1)        # else bit is "1"
     	j printBin
     	printZero:
     	print_int(0)
     	
     	j printBin
 end:
-	li $v0, 10
+	li $v0, 10      # Exit program
 	syscall
 
 bits_read:
-    li $t9, 0   #counter
-    li $v0, 0   #result
+    # $a0 = starting array element, $a1 = offset, $a2 = nbits
+    li $t9, 0   #$t9 = counter of bits checked
+    li $v0, 0   #$v0 = result
     loop:
-        lb		$t1, ($a0)
-        li		$t0, 128
-        srlv		$t0, $t0, $a1
-        and		$t3, $t1, $t0
-        addi		$a1,$a1,1
-        bnez    $t3, changeRes
-        rest:
-        addi    $t9,$t9,1
-        bge	$t9,$a2,exit
-        bgtz	$t0, loop
-        addi	$t9,$t9,-1
+        lb		$t1, ($a0)   # $t1 = $a0
+        li		$t0, 128    # our mask $t0 = 1000 0000
+        srlv	$t0, $t0, $a1   # shift mask offset times
+        and		$t3, $t1, $t0   # $t3 = $t1 and $t0
+        addi	$a1,$a1,1       # increase offset by one 
+        bnez    $t3, changeRes  # if $t3 != 0 then we need to update our bits result with the new 1 bit
+    rest:
+        addi    $t9,$t9,1       # add counter by one
+        bge	    $t9,$a2,exit    # if counter == nbits we have finished
+        bgtz	$t0, loop       # if mask == 0 we need to refresh it to 1000 0000
+        addi	$t9,$t9,-1      # counter should not be increased in case we are at the step of switching elements
         
-        li	$a1,0
-        addi 	$a0,$a0,1
-        blt     $t9,$a2,loop
-        exit:
+        li	    $a1,0          # next element should be read from the MSB 
+        addi 	$a0,$a0,1      # icrease $a0 to the next array element
+        blt     $t9,$a2,loop   # return to loop if $t9 is less than nbits
+    exit:
         jr      $ra
     changeRes:
         sub $t7,$a2,$t9
         addi $t7,$t7,-1
         li $t6,1
-        sllv $t6,$t6,$t7
-        or $v0,$v0,$t6
+        sllv $t6,$t6,$t7       # set the new bit to its proper location,(nbits - counter - 1) from the right
+        or $v0,$v0,$t6         # perform or to add the new bit to the result in the correct position
         j rest
