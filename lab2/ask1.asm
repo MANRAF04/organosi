db164acaa125b5eb8ee2bd63a49b27e8bdedc0d3
manraf <45131457+MANRAF04@users.noreply.github.com>
2023-11-10 00:10:41 +0200

    Add files via upload

--- lab2/ask1.asm
+++ lab2/ask1.asm
@@ -41,6 +41,14 @@ end:
 	PRINT_STR("END OF ARRAY\n")
 .end_macro
 
+.macro ADD_TO_OUT(%lessnum,%lessarr,%lesscount)
+meth:
+	sw %lessnum,($t0)
+	addi $t0,$t0,4
+	addi %lesscount,%lesscount,1
+	addi %lessarr,%lessarr,4
+.end_macro
+
 .data
 prompt: .asciiz "Enter an integer: "
 .align 2
@@ -53,26 +61,79 @@ out: .space 2000
 
 .text
 main:
-    	la $a0, prompt
-    	li $v0, 4
-    	syscall
+  	
+  	PRINT_STR("\tArray 1\nEnter in1 size:")
   	
   	READ_INT
 	move $s1,$v0
 	la $s0,in1
 	READ_INT_ARR($s0,$s1)
 	la $s0,in1
-	PRINT_STR("\tArray 1\n")
-	PRINT_INT_ARR($s0,$s1)
+	#PRINT_STR("\tArray 1\n")
+	#PRINT_INT_ARR($s0,$s1)
 
+	PRINT_STR("\tArray 2\nEnter in2 size:")
   	READ_INT
 	move $s3,$v0
 	la $s2,in2
 	READ_INT_ARR($s2,$s3)  
   	la $s2,in2
-  	PRINT_STR("\tArray 2\n")
-  	PRINT_INT_ARR($s2,$s3)  	
+  	#PRINT_STR("\tArray 2\n")
+  	#PRINT_INT_ARR($s2,$s3)  	
+	la $a0,in1	#In1
+	move $a1,$s1	#Size1
+	la $a2,in2	#In2
+	move $a3,$s3	#Size2
+	
+	la $s4,out
+	add $s5,$s1,$s3
+	
+	addi $sp,$sp,-4
+	sw $s4,($sp)
+	
+	jal merge
+
+	addi $sp,$sp,4
+  	PRINT_STR("\tOut Array\n")
+  	PRINT_INT_ARR($s4,$s5)  	
   	
 exit:
 	li $v0, 10
 	syscall
+
+merge:
+	lw $t0,($sp)	#Out arr
+	li $t1,0	#Counter for in1
+	li $t2,0	#Counter for in2
+	
+	loop:
+		beq $t1,$a1,rest2
+				
+		beq $t2,$a3,rest1
+		lw $t3,($a0)
+		lw $t4,($a2)
+		
+		beq $t3,$t4,both
+		bgt $t3,$t4,oneBigger
+		ADD_TO_OUT($t3,$a0,$t1)
+		j loop
+	oneBigger:
+		ADD_TO_OUT($t4,$a2,$t2)
+		j loop
+	both:
+		ADD_TO_OUT($t3,$a0,$t1)
+		ADD_TO_OUT($t4,$a2,$t2)
+		j loop
+	rest1:
+		beq $t1,$a1,end
+		lw $t3,($a0)
+		ADD_TO_OUT($t3,$a0,$t1)
+		j rest1
+	rest2:
+		beq $t2,$a3,end
+		lw $t4,($a2)
+		ADD_TO_OUT($t4,$a2,$t2)
+		j rest2
+
+	end:
+		jr $ra
