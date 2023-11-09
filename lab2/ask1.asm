efb75863079c470bfec223a39250b331aed3aced
Zachariadis Charis <124374442+czachariadis@users.noreply.github.com>
2023-11-10 01:12:48 +0200

    Update ask1.asm
    with comments

--- lab2/ask1.asm
+++ lab2/ask1.asm
@@ -1,3 +1,4 @@
+# MACROS
 .macro READ_INT
 li $v0, 5
 syscall
@@ -13,41 +14,44 @@ str: .asciiz %s
 .end_macro
 
 .macro READ_INT_ARR (%arr,%size)
-li $t4,0
+li $t4,0    # $t4 = index for read the array's elements
 
 loop:
-	bge $t4,%size,end
+	bge $t4,%size,end   # if $t4 >= size we reached the end
 	READ_INT
 	move $t3,$v0
-	sw $t3,0(%arr) 
-	addi %arr,%arr,4
-	addi $t4,$t4,1
+	sw $t3,0(%arr)      # store the int to the array's index position
+	addi %arr,%arr,4    # go to the next int spot
+	addi $t4,$t4,1      # add index by one
 	j loop 
 end:
 .end_macro
 
 .macro PRINT_INT_ARR (%arr,%size)
-li $t4,0
+li $t4,0    # $t4 = index for read the array's element
 loop:
-	bge $t4,%size,end
-	lw  $a0,0(%arr) 
-	addi %arr,%arr,4
-	addi $t4,$t4,1
+	bge $t4,%size,end   # if $t4 >= size we reached the end
+	lw  $a0,0(%arr)     # prepare the int element on the index position to be printed
+	addi %arr,%arr,4    # go to the next int spot
+	addi $t4,$t4,1      # add index by one
 	li $v0,1
-	syscall
+	syscall             #print the int with and change line
 	PRINT_STR("\n")
 	j loop 
 end:
 	PRINT_STR("END OF ARRAY\n")
 .end_macro
 
+# A Macro to put a new element to the output array
 .macro ADD_TO_OUT(%lessnum,%lessarr,%lesscount)
+# lessnum = element int, lessarr = element's array, lesscount = elements' index
 meth:
-	sw %lessnum,($t0)
-	addi $t0,$t0,4
-	addi %lesscount,%lesscount,1
-	addi %lessarr,%lessarr,4
+	sw %lessnum,($t0)       # store the element to the output array
+	addi $t0,$t0,4          # go to the next int spot of the merged array
+	addi %lesscount,%lesscount,1    # add index by one
+	addi %lessarr,%lessarr,4    # go to the next int spot of the elements' array
 .end_macro
+
 
 .data
 prompt: .asciiz "Enter an integer: "
@@ -66,74 +70,72 @@ main:
   	
   	READ_INT
 	move $s1,$v0
-	la $s0,in1
+	la $s0,in1                  # store in1 array from the user to $s0
 	READ_INT_ARR($s0,$s1)
 	la $s0,in1
-	#PRINT_STR("\tArray 1\n")
-	#PRINT_INT_ARR($s0,$s1)
 
 	PRINT_STR("\tArray 2\nEnter in2 size:")
   	READ_INT
 	move $s3,$v0
-	la $s2,in2
+	la $s2,in2                  ## store in1 array from the user to $s0
 	READ_INT_ARR($s2,$s3)  
   	la $s2,in2
-  	#PRINT_STR("\tArray 2\n")
-  	#PRINT_INT_ARR($s2,$s3)  	
-	la $a0,in1	#In1
-	move $a1,$s1	#Size1
-	la $a2,in2	#In2
-	move $a3,$s3	#Size2
+
+	la $a0,in1	    
+	move $a1,$s1	      # Prepare arguments for the merge function call
+	la $a2,in2	    
+	move $a3,$s3	
+
+	la $s4,out          # store the output array's pointer in $s4
+	add $s5,$s1,$s3     # store the output array's size in $s5
 	
-	la $s4,out
-	add $s5,$s1,$s3
-	
-	addi $sp,$sp,-4
-	sw $s4,($sp)
+	addi $sp,$sp,-4     # make room for the array pointer in the stack
+	sw $s4,($sp)        # store the output array pointer to the stack
 	
 	jal merge
 
-	addi $sp,$sp,4
+	addi $sp,$sp,4      # release the used room in the stacked
   	PRINT_STR("\tOut Array\n")
-  	PRINT_INT_ARR($s4,$s5)  	
+  	PRINT_INT_ARR($s4,$s5)  	    # print the output array
   	
 exit:
-	li $v0, 10
+	li $v0, 10      # exit program
 	syscall
 
 merge:
-	lw $t0,($sp)	#Out arr
-	li $t1,0	#Counter for in1
-	li $t2,0	#Counter for in2
+# $a0 = in1, $a1 = size1, $a2 = in2 $a3 = size2, stack1 = output array pointer 
+
+	lw $t0,($sp) # $t0 = Out arr
+	li $t1,0	# counter1 for in1
+	li $t2,0	# counter2 for in2
 	
 	loop:
-		beq $t1,$a1,rest2
-				
-		beq $t2,$a3,rest1
-		lw $t3,($a0)
-		lw $t4,($a2)
+		beq $t1,$a1,rest2   #if counter1 == size2, then put the rest in2 elements to the merged array 			
+		beq $t2,$a3,rest1   #if counter2 == size, then put the rest in1 elements to the merged array
+		lw $t3,($a0)        # $t3 = in1[0]
+		lw $t4,($a2)        # $t4 = in2[0]
 		
-		beq $t3,$t4,both
-		bgt $t3,$t4,oneBigger
-		ADD_TO_OUT($t3,$a0,$t1)
+		beq $t3,$t4,both            # if in1[i] == in2[i], then put both elements to merged array
+		bgt $t3,$t4,oneBigger       # if in1[i] > in2[i], then go to oneBigger branch
+		ADD_TO_OUT($t3,$a0,$t1)     # if in1[i] < in2[i], add in1[i] to the merged array
 		j loop
 	oneBigger:
-		ADD_TO_OUT($t4,$a2,$t2)
+		ADD_TO_OUT($t4,$a2,$t2)     # add in2[i] to the merged array
 		j loop
 	both:
-		ADD_TO_OUT($t3,$a0,$t1)
-		ADD_TO_OUT($t4,$a2,$t2)
+		ADD_TO_OUT($t3,$a0,$t1)     # add in1[i] to the merged array
+		ADD_TO_OUT($t4,$a2,$t2)     # add in2[i] to the merged array
 		j loop
 	rest1:
-		beq $t1,$a1,end
-		lw $t3,($a0)
-		ADD_TO_OUT($t3,$a0,$t1)
-		j rest1
+		beq $t1,$a1,end     # if counter1 == size1, go to the end
+		lw $t3,($a0)        # load the current element of in1 to $t3
+		ADD_TO_OUT($t3,$a0,$t1)     # add the element to the merged array
+		j rest1     # repeat until you reached the end of in1
 	rest2:
-		beq $t2,$a3,end
-		lw $t4,($a2)
-		ADD_TO_OUT($t4,$a2,$t2)
-		j rest2
+		beq $t2,$a3,end     # if counter2 == size2, go to the end
+		lw $t4,($a2)        # load the current element of in2 to $t4
+		ADD_TO_OUT($t4,$a2,$t2)     # add the element to the merged array
+		j rest2     # repeat until you reached the end of in2
 
 	end:
 		jr $ra
