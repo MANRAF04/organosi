df83dee16fb769c58a8ad9a6718416ebb2790e80
manraf <45131457+MANRAF04@users.noreply.github.com>
2023-12-11 19:58:38 +0200

    Add files via upload

--- Lab 5/control.v
+++ Lab 5/control.v
@@ -1,19 +1,20 @@
+`include "constants.vh"
 // Module to control the data path. 
 //                          Input: op, func of the inpstruction
 //                          Output: all the control signals needed 
 module ControlUnit(
            input [5:0] opcode, 
            input [5:0] func,
-           output [3:0] aluOp,
-           output rWrite,
-           output rDst,
-           output aluSrc,
-           output branch,
-           output bequal,
-           output memWrite,
-           output memToReg
+           output reg [3:0] aluOp,
+           output reg rWrite,
+           output reg rDst,
+           output reg aluSrc,
+           output reg branch,
+           output reg bequal,
+           output reg memWrite,
+           output reg memRead,
+           output reg memToReg
            );
-`include "constants.vh"
 		   
 // Write the FSM code here
 always @(*) begin
@@ -24,50 +25,62 @@ always @(*) begin
     memWrite = 0; 
     memToReg = 0;
     bequal = 0; 
+    memRead = 0;
+
     case (opcode)
-        R_FORMAT:
+        (`R_FORMAT): begin 
             rWrite = 1; 
             rDst = 1; 
-        LW:
-             rWrite = 1; 
-             aluSrc = 1; 
-             memToReg = 1; 
-             aluOp = 4'b0010;
-        SW:
-             rWrite = 1; 
-             rDst = 1'bx; 
-             aluSrc = 1; 
-             memWrite = 1; 
-             memToReg = 1'bx; 
-             aluOp = 4'b0010;
-        BNE:
-             rWrite = 1; 
-             rDst = 1; 
-             aluOp = 4'b0110;
-        BEQ:
-             rWrite = 1; 
-             rDst = 1; 
-             aluOp = 4'b0110;
-             bequal = 1;
-        ADDI:
-             rWrite = 1; 
-             aluSrc = 1; 
-             aluOp = 4'b0010; 
-        default: 
+        end
+        (`LW): begin
+            rWrite = 1; 
+            aluSrc = 1; 
+            memToReg = 1; 
+            aluOp = 4'b0010;
+        end
+        (`SW): begin
+            rDst = 1'bx; 
+            aluSrc = 1; 
+            memWrite = 1; 
+            memToReg = 1'bx; 
+            aluOp = 4'b0010;
+        end
+        (`BNE): begin
+            rWrite = 1; 
+            rDst = 1'bx;
+            branch = 1'b1;
+            memToReg = 1'bx;
+            aluOp = 4'b0110;
+        end
+        (`BEQ): begin
+            rWrite = 1; 
+            rDst = 1'bx;
+            branch = 1'b1;
+            memToReg = 1'bx; 
+            aluOp = 4'b0110;
+            bequal = 1;
+        end
+        (`ADDI): begin
+            rWrite = 1; 
+            aluSrc = 1; 
+            aluOp = 4'b0010;
+            end 
+        default:
+            aluOp = 4'bx;
     endcase
 end
 
 always @(*) begin
     case (func)
-        ADD:
+        (`ADD):
             aluOp = 4'b0010;
-        SUB:
+        (`SUB):
             aluOp = 4'b0110;
-        OR:
+        (`OR):
             aluOp = 4'b0001;
-        AND:
+        (`AND):
             aluOp = 4'b0000;
-        SLT:
+        (`SLT):
             aluOp = 4'b0111;
         default: aluOp = 4'bx; 
     endcase
--- Lab 5/cpu.v
+++ Lab 5/cpu.v
@@ -4,37 +4,48 @@
 
 module CPU(clock,reset);
 
-    reg [31:0] PC;
-    reg inst_wen, inst_ren;
+    reg [31:0] PC = 32'b0;
+    reg inst_wen = 0, inst_ren = 1;
     input clock, reset;
-    wire [31:0] din, dout; 
-    wire [5:0] wreg;
+    wire [31:0] din, dout, mdout; 
+    reg [4:0] wreg;
     wire [3:0] aluOp;
-    wire rWrite, rDst, aluSrc, branch, bequal, memWrite, memToReg;
-    wire [4:0] rdA, rdB;
-    wire [31:0] wd;
+    wire rWrite, rDst, aluSrc, branch, bequal, memWrite, memToReg, memRead;
+    wire [31:0] rdA, rdB;
+    wire [31:0] aluOut;
+    reg [31:0] signXt, inB, wd;
+    wire zero;
 
     always @(negedge clock) begin
-        PC = PC + 1;
+        PC = PC + 4;
     end
 
     always @(dout) begin   // MUX for wreg
         wreg = (rDst) ? dout[15:11] : dout[20:16];
+        signXt = dout[15:0] << 1;
     end
 
-    assign PC = 32'b0;
-    assign inst_ren = 1;
-    assign inst_wen = 0;
+    always @(rdB, signXt) begin // MUX for inB
+        inB = (aluSrc) ? (signXt) : (rdB);
+    end
 
-    Memory InstMemory(.clock(clock),.reset(reset),.ren(inst_ren),.wen(inst_wen),.addr(PC),.din(din),.dout(dout));
-    RegFile cpu_regs(.clock(clock), .reset(reset), .raA(dout[25:21]), .raB(dout[20:16]), .wa(wreg), .wen(rWrite), .wd(), .rdA(rdA), .rdB(rdB)); // for next
-    ControlUnit cUnit(.opcode(dout[31:26]),.func(dout[5:0]),.aluOp(aluop), .rWrite(rWrite), .rDst(rDst), .aluSrc(aluSrc),
-                  .branch(branch), .bequal(bequal), .memWrite(memWrite), .memToReg(memToReg));
-    ALU alu ();
+    always @(aluOut, mdout) begin // MUX for write data
+        wd = (memToReg) ? (aluOut) : (mdout);
+    end
+    
+    always @(PC,signXt) begin
+        if ((zero^dout[26]) & branch) begin //Alliws zero^bequal 8a to doyme
+            PC = PC + signXt<<2;
+        end
+    end
 
-
-
-
+    Memory InstMemory(.clock(clock),.reset(reset),.ren(inst_ren),.wen(inst_wen),.addr({2'b0, PC[31:2]}),.din(din),.dout(dout));
+    RegFile cpu_regs(.clock(clock), .reset(reset), .raA(dout[25:21]), .raB(dout[20:16]), .wa(wreg), .wen(rWrite), .wd(wd), .rdA(rdA), .rdB(rdB));
+    ControlUnit cUnit(.opcode(dout[31:26]),.func(dout[5:0]),.aluOp(aluOp), .rWrite(rWrite), .rDst(rDst), .aluSrc(aluSrc),
+                      .branch(branch), .bequal(bequal), .memWrite(memWrite), .memToReg(memToReg), .memRead(memRead));
+    ALU #(32) alu(.out(aluOut),.zero(zero), .inA(rdA), .inB(inB), .op(aluOp));
+    Memory DataMemory(.clock(clock), .reset(reset), .ren(memRead), .wen(memWrite),.addr(aluOut),.din(rdB),.dout(mdout));
+    
 
 
 endmodule
